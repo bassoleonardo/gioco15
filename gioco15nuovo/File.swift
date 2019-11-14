f060005c58f9f34751ce7e26fefcc7cdc86d4123
leonardo <leonardo.basso@barsanti.edu.it>
2019-11-14 08:35:51 +0100

    creazione classe cella

---
+++ gioco15nuovo/File.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  gioco15nuovo
+//
+//  Created by Leonardo Basso on 14/11/2019.
+//  Copyright © 2019 Leonardo Basso. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class cella
+{
+    var riga: Int = 0;
+    var colonna: Int = 0;
+    var zero: Int = 0;
+    func metadone()
+    {
+       print("hello")
+    }
+}
