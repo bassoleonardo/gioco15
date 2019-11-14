89129433586ae671ada120e2b34491e45c48ceb9
leonardo <leonardo.basso@barsanti.edu.it>
2019-11-14 09:09:01 +0100

    array

--- gioco15nuovo/File.swift
+++ gioco15nuovo/File.swift
@@ -14,7 +14,10 @@ class cella
     var riga: Int = 0;
     var colonna: Int = 0;
     var zero: Int = 0;
-    var arr: [[Int]] = []
+    var arr: [[Int]] = [[1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16]]
     func metadone()
     {
        print("hello")
